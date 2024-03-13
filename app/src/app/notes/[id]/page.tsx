57443d336cd5898e669cd3cfc9c86aa209f9ea8d
YoheiKugue <yohei.kugue@excite.jp>
2024-03-13 14:54:49 +0900

    first commit

--- app/src/app/notes/[id]/page.tsx
+++ app/src/app/notes/[id]/page.tsx
@@ -1,13 +1,13 @@
-import { Flex } from '@radix-ui/themes'
+import { Card, Flex } from '@radix-ui/themes'
 
 export default function Page({ params }: { params: { id: string } }) {
   return (
-    <div>
-      My Post: {params.id}
-      <Flex height="100%">
-        <div></div>
-        <div></div>
+    <main>
+      <Flex direction="column" justify="center" align="center" height="100%">
+        <Card variant="classic">
+          <Flex p="5">Note ID: {params.id}</Flex>
+        </Card>
       </Flex>
-    </div>
+    </main>
   )
 }
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,12 +1,19 @@
 import Button from '@/components/Button/Button'
-import { Flex } from '@radix-ui/themes'
+import { Card, Flex } from '@radix-ui/themes'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
-      <Flex direction="column" pb="3" width="max-content">
-        <h1>Hello World</h1>
-        <Button>開始</Button>
+      <Flex direction="column" justify="center" align="center" height="100%">
+        <Card variant="classic">
+          <Flex direction="column" justify="center" align="center" height="100%" p="5">
+            <h1>Hello World</h1>
+            <Link href="/notes">
+              <Button>Start</Button>
+            </Link>
+          </Flex>
+        </Card>
       </Flex>
     </main>
   )
